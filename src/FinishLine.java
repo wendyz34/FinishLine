dd1431e0867d4e80ef6b3bc6f4edd5aefa7c1643
wendy <wendyz34@nycstudents.net>
2023-02-20 14:22:55 -0500

    print

--- src/FinishLine.java
+++ src/FinishLine.java
@@ -13,6 +13,32 @@ public class FinishLine {
         return randNum;
     }
     public int determineWin(){
+        while(pos1<=10||pos2<=10) {
+            int dice = randNum();
+            int dice2 = randNum();
+            System.out.println("Dice: " + dice);
+            System.out.println("Dice2: " + dice);
+            if (dice + dice2 == pos1||dice==pos1||dice2==pos1) {
+                pos1++;
+            }
+            System.out.println("Position 1: " + pos1);
+            if(pos1==10){
+                break;
+            }
+            //return pos1;
+            int d1 = randNum();
+            int d2 = randNum();
+            System.out.println("Dice: " + dice);
+            System.out.println("Dice2: " + dice);
+            if (d1 + d2 == pos2||d1==pos2||d2==pos2) {
+                pos2++;
+            }
+            System.out.println("Position 2: " + pos2);
+            if(pos1==10){
+                break;
+            }
+        }
+        return pos2;
 
     }
     /*public int determineWin(){
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         FinishLine player1 = new FinishLine(1);
-        FinishLine player2 = new FinishLine(1);
+        //FinishLine player2 = new FinishLine(1);
         boolean turn = true;
         boolean turn2 = false;
         System.out.println("Roll the dice to see which player wins");
-        while(player1.determineWin()!=10||player2.determineWin2()!=10){
+        while(player1.determineWin()<=10){
             player1.determineWin();
-            player2.determineWin2();
         }
         player1.endGame();
         //player2.endGame();
